045e3ffe92b23bbf7e6a85b88d3d002e5d413d38
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-10-11 15:58:31 +0100

    site styling

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-rooms',
@@ -29,5 +30,32 @@ export class RoomsComponent implements OnInit {
       this.room = dataService;
       console.log(this.room);
     });
+
+    let roomId = this.id;
+    switch (roomId) {
+      case 1: {
+        document.body.style.backgroundImage =
+          "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url('../../../assets/img/board.jpg')";
+        break;
+      }
+      case 2: {
+        document.body.style.backgroundImage =
+          "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url('../../../assets/img/green.jpg')";
+        break;
+      }
+      case 3: {
+        document.body.style.backgroundImage =
+          "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url('../../../assets/img/cave.jpg')";
+        break;
+      }
+      default: {
+        document.body.style.background = '#fff';
+        break;
+      }
+    }
+
+    // if (this.id === 1) {
+    //   document.body.style.background = 'yellow';
+    // }
   }
 }
